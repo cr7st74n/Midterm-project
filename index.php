9706c4c8fefe162df9ccb96e7f8cb92a6212b4a5
cr7st74n <cris-082011@hotmail.com>
2024-11-02 22:14:50 -0400

    new structure defined , and basic styles to the page

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("./view/head.php"); ?>
+
+<body>
+    <?php include('./view/header.php'); ?>
+
+    <main class="container">
+        <h1>Main Content</h1>
+        <P>
+            John's Resort offers a special lodging experience on the New Jersey Eastern Coast.
+            Relax in serenity with Panoramic views of the Atlantic Ocean.
+            • Private yurts with decks overlooking the ocean
+            • Activities lodge with fireplace and gift shop
+            • Nightly fine dining at the Overlook Cafe
+            • Heated outdoor pool and whirlpool
+            • Guided hiking tours of the redwoods
+            John's Resort
+            One Normal Ave, Montclair
+            New Jersey NJ 07043
+            973-655-4166
+        </P>
+    </main>
+
+    <?php include("./view/footer.php"); ?>
+</body>
+
+</html>
